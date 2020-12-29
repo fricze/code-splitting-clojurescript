46fcefcdbb4ca06d6106ffe5d689b023db11719a
jonki <andrzej.fricze@gmail.com>
2020-12-29 18:35:41 +0100

    Remove React Suspense and React lazy in favor of CLJS solution. Fixes props problem

--- src/main/demo/app.cljs
+++ src/main/demo/app.cljs
@@ -1,17 +1,17 @@
 (ns demo.app
-  (:require
-    ["react" :as react]
-    [reagent.core :as r]
-    [demo.env :as env]
-    [demo.util :refer (lazy-component)]))
+  (:require ["react" :as react]
+            [reagent.core :as r]
+            [demo.env :as env]
+            [shadow.lazy :refer [loadable] :as lazy]
+            [demo.util :refer [lazy-component]]))
 
 (defonce root-el (js/document.getElementById "root"))
 
-(def product-detail (lazy-component demo.components.product-detail/root))
-(def product-listing (lazy-component demo.components.product-listing/root))
-(def sign-in (lazy-component demo.components.sign-in/root))
-(def sign-up (lazy-component demo.components.sign-up/root))
-(def account-overview (lazy-component demo.components.account-overview/root))
+(def product-detail (lazy-component (loadable demo.components.product-detail/root)))
+(def product-listing (lazy-component (loadable demo.components.product-listing/root)))
+(def sign-in (lazy-component (loadable demo.components.sign-in/root)))
+(def sign-up (lazy-component (loadable demo.components.sign-up/root)))
+(def account-overview (lazy-component (loadable demo.components.account-overview/root)))
 
 (defn welcome []
   [:h1 "Welcome to my Shop!"])
@@ -25,8 +25,7 @@
        [:li [:a {:href "#" :on-click #(swap! env/app-state assoc :page :account-overview)} "My Account"]]
        [:<>
         [:li [:a {:href "#" :on-click #(swap! env/app-state assoc :page :sign-in)} "Sign In"]]
-        [:li [:a {:href "#" :on-click #(swap! env/app-state assoc :page :sign-up)} "Sign Up"]]
-        ])]))
+        [:li [:a {:href "#" :on-click #(swap! env/app-state assoc :page :sign-up)} "Sign Up"]]])]))
 
 (defn root []
   (let [{:keys [page] :as state} @env/app-state]
@@ -36,28 +35,28 @@
 
      [nav {}]
 
-     [:> react/Suspense {:fallback (r/as-element [:div "Loading ..."])}
-      (case page
-        :product-listing
-        [:> product-listing]
+     (case page
+       :product-listing
+       [product-listing {:fallback (fn [] [:div "waiting for product"])}]
 
-        :product-detail
-        [:> product-detail {}]
+       :product-detail
+       [product-detail {}]
 
-        :sign-in
-        [:> sign-in {}]
+       :sign-in
+       [sign-in {}]
 
-        :sign-up
-        [:> sign-up {:x "foo"}]
+       :sign-up
+       [sign-up {:x "foo"}]
 
-        :account-overview
-        [:> account-overview {}]
+       :account-overview
+       [account-overview {}]
 
-        :welcome
-        [welcome {}]
+       :welcome
+       [welcome {}]
 
-        [:div "Unknown page?"]
-        )]]))
+       [:div "Unknown page?"])
+
+     ]))
 
 (defn ^:dev/after-load start []
   (r/render [root] root-el))
